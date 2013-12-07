e828c2c7051f8bab52ac5c29bff5704fba22803f
James Saxon <jsaxon@cars.com>
2013-12-06 16:36:28 -0800

    UI Router version starting to work.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,41 +1,103 @@
 'use strict';
 
-var myApp = angular.module('sillyApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
-    'ngAnimate'
-    'ui.router'
-])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/pages/main.html',
-                controller: 'MainCtrl'
+var app = angular
+
+    .module('sillyApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngAnimate',
+        'ui.router',
+    ])
+
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('home', {
+                templateUrl: 'views/pages/main.html'
             })
-            .when('/home', {
-                templateUrl: 'views/pages/main.html',
-                controller: 'MainCtrl'
+            .state('about', {
+                templateUrl: 'views/pages/about.html'
             })
-            .when('/about', {
-                templateUrl: 'views/pages/about.html',
-                controller: 'MainCtrl'
-            })
-            .when('/contact', {
-                templateUrl: 'views/pages/contact.html',
-                controller: 'MainCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
+            .state('contact', {
+                templateUrl: 'views/pages/contact.html'
             });
     })
+
     .filter('reverse', function() {
         return function(items) {
             return items.slice().reverse();
         };
     })
-;
+
+    .factory('stuff', function () { 
+        return  { 
+            yes: 9999,
+            getYes: function () { 
+                console.log("Getyes INTERNAL: " + this.yes);
+                return this.yes; 
+            }
+        }
+    })
+
+    .factory('nav', function ($state) {
+        return {
+            tabs: [
+                {name:'Home', state:"home"},
+                {name:'Contact', state:"contact"},
+                {name:'About', state:"about"}
+            ],
+            start: 'home'
+        }
+    })
+
+    .controller('MainCtrl', function ($scope, $state, nav, stuff) {
+        //console.log("MAIN CONTROLLER");
+        //console.log($route.current.$$route.originalPath);
+        //console.log($route.current.params);
+        //console.log($location);
+        //console.log($routeParams);
+        // This only seems to be getting called once...  
+        $scope.nav_tabs = nav.tabs;
+        console.log($scope.nav_tabs);
+        console.log($state);
+        $state.transitionTo(nav.start);
+        console.log($state.current.name);
+        $scope.$on('$stateChangeSuccess', function (event, toState, fromState, fromParams) {
+            console.log(toState);
+            $scope.page = toState.name;
+            if (toState.name == 'home') { console.log("GOING HOME!!!!"); }
+        });
+    })
+
+    .controller('SplendidCtrl', function ($scope, stuff, nav) {
+        $scope.splendid_count = 0;
+        $scope.max_splendid_count = 2;
+        $scope.stuff = stuff;
+        $scope.shmoopy = "What do you think?";
+        $scope.blah = function () {
+            $scope.shmoopy = $scope.shmoopy.split("").reverse().join("");
+            $scope.splendid_count += 1;
+            $scope.stuff.yes -= 1;
+        }
+    })
+
+    ;
+// Might need to do a watch on the service or
+
+// restructure things...  easy... but with services...
+
+
+//Watching things like this:
+
+//$scope.$watch( 
+//    // WHAT TO WATCH
+//    function () { return siteDomain.domainSelected(); },   
+//    // If the thing to watched changed, do this with the new value.
+//    function (domain_selected) { $scope.domain_selected = domain_selected;},  
+//    // DEEP INTROSPECTION
+//    true 
+//);
+//    
 
 /*
 I can change this to a ui-router which would allow the view in view situation.
